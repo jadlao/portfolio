763e4e011cd3379300eae3f6cd4c5bcd5dacf834
Josef Adlao <jadlao@gmail.com>
2018-06-21 17:10:19 +1000

    rearranged portfolio

--- assets/js/App/data/portfolioData.js
+++ assets/js/App/data/portfolioData.js
@@ -7,20 +7,6 @@ var portfolioData = [
     image: 'https://image.ibb.co/mRRA8y/thumb_luxrentals.png'
   },
   {
-    title: 'MySatoshiConverter',
-    live_link: 'https://mysatoshiconverter.com/',
-    code_link: 'https://mysatoshiconverter.com/',
-    technologies: 'React, SASS',
-    image: 'https://image.ibb.co/eUnZjn/thumb_mysatoshi.png'
-  },
-  {
-    title: 'Olooq Landing Page',
-    live_link: 'https://jadlao-olooq.surge.sh/',
-    code_link: 'https://github.com/jadlao/olooqLandingPage',
-    technologies: 'Bootstrap, jQuery',
-    image: 'https://image.ibb.co/nq93c7/thumb_olooq.png'
-  },
-  {
     title: 'Spotify Clone',
     live_link: 'https://jadlao-spotify.surge.sh/',
     code_link: 'https://github.com/jadlao/spotify-clone',
@@ -33,6 +19,20 @@ var portfolioData = [
     code_link: 'https://github.com/jadlao/likehome',
     technologies: 'HTML5, CSS3',
     image: 'https://image.ibb.co/nBAWrc/thumb_likehome.png'
+  },
+  {
+    title: 'MySatoshiConverter',
+    live_link: 'https://mysatoshiconverter.com/',
+    code_link: 'https://mysatoshiconverter.com/',
+    technologies: 'React, SASS',
+    image: 'https://image.ibb.co/eUnZjn/thumb_mysatoshi.png'
+  },
+  {
+    title: 'Olooq Landing Page',
+    live_link: 'https://jadlao-olooq.surge.sh/',
+    code_link: 'https://github.com/jadlao/olooqLandingPage',
+    technologies: 'Bootstrap, jQuery',
+    image: 'https://image.ibb.co/nq93c7/thumb_olooq.png'
   }
 ];
 
