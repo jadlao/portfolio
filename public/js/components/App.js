0ba7403766f0b48c87a4aa9056fd5c61be0f2a5a
Josef Adlao <jadlao@gmail.com>
2018-06-28 17:56:39 +1000

    edited package json

--- public/js/components/App.js
+++ public/js/components/App.js
@@ -777,7 +777,7 @@ var data = [{
   }
 }, {
   job_title: 'Web Developer',
-  home_text: "Hi, my name is Josef. This is my page and I'm a self-taught web developer from Sydney, Australia. Coming from a background in science, I naturally enjoy solving problems and the tech side of things. I love using React for the frontend, and am currently learning more about the backend using PHP and Laravel."
+  home_text: "Hi, my name is Josef. This is my site and I'm a self-taught web developer from Sydney, Australia. Coming from a background in science, I naturally enjoy solving problems and the tech side of things. I love using React for the frontend, and am always eager to learn more technologies."
 }];
 
 exports.default = data;
