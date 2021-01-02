efca0771d1c363ced612f474998899e3129bf799
Josef Adlao <jadlao@gmail.com>
2021-01-03 10:48:59 +1100

    edited description

--- assets/js/components/data/data.js
+++ assets/js/components/data/data.js
@@ -11,7 +11,7 @@ var data = [
 		job_title: 'Web Developer',
 		about_h4text: "Hello, I'm Josef, a Web Developer based in Sydney",
 		about_text:
-			"With a background in nanotech, I naturally enjoy solving problems and  digging in to the technical side of things. Since then I have loved learning new technologies such as frontend frameworks like React, and am always eager to learn more. Currently, I'm working at Foxtel Media as a Digital Advertising Developer and am learning a lot about Ad-tech and the advertising ecosystem. When I am not coding, you can find me trying new restaurants, reading literature, or listening to the soothing melodies of Ludovico Einaudi."
+			"With a background in nanotech, I naturally enjoy solving problems and  digging in to the technical side of things. Since then I have loved learning new technologies such as frontend frameworks like React, and am always eager to learn more. Currently, I'm working at Foxtel Media as an Ad Tech Specialist. When I am not coding, you can find me trying new restaurants, reading literature, or playing around with my kitten Pearl."
 	}
 ];
 
